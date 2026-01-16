d00b61fbd4c275be66afcff681e5cb3cbf887951
buramanta <sayapinterbuatakun@gmail.com>
2026-01-16 17:09:14 +0700

    minor change

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
@@ -6,14 +7,20 @@ def clean_data(df):
     Cleaning Dataset
     """
     df = df[
-        ['title', 'description', 'images', 'type',
-         'episodes', 'released_season', 'release_year']
+        ['title',
+         'description',
+         'image',
+         'Type',
+         'Episodes',
+         'Released_Season',
+         'Released_Year']
     ]
     
     df = df.dropna(subset=['description'])
-    df['episodes'] = df['episodes'].fillna(0)
-    df['released_season'] = df['released_season'].fillna('Unknown')
-    df['type'] = df['type'].fillna('Unknown')
+    df['Episodes'] = df['Episodes'].fillna(0)
+    df['Type'] = df['Type'].fillna('Unknown')
+    df['Released_Season'] = df['Released_Season'].fillna('Unknown')
+    df['Released_Year'] = df['Released_Year'].fillna('Unknown')
     
     return df
 
@@ -22,18 +29,14 @@ def get_image_url(image_col):
     Extract image URL from the images column
     """
     try:
-        return eval(image_col)['jpg']['image_url']
+        return json.loads(image_col)['jpg']['image_url']
     except:
         return "https://via.placeholder.com/300x450"
     
-def vectorize_descriptions(text_series, vectorizer=None):
+def vectorize_descriptions(text_series):
     """
     TF-IDF Vectorization
     """
-    
-    if vectorizer:
-        return vectorizer.transform(text_series)
-    
     tfidf = TfidfVectorizer(
         stop_words='english',
         max_features=5000
