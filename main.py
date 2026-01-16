0b43908f234bd57d70debc1428f3a1bfa1dbcaad
buramanta <sayapinterbuatakun@gmail.com>
2026-01-16 19:34:01 +0700

    minor change again

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from utils.preprocess import clean_data, get_image_url
 import joblib
 
 # Page configuration
-
 st.set_page_config(
     page_title ="Anime Recommendation System",
     layout="wide"
@@ -14,30 +13,25 @@ st.title("Anime Recommendation System")
 st.write("Rekomendasi anime untuk pemula berdasarkan deskripsi.")
 
 # Load Dataset
-
 @st.cache_data
 def load_data():
-    df = pd.read_csv("data/mal_anime.csv")
-    return df
+    return pd.read_csv("data/mal_anime.csv")
 
 df = load_data()
 df = clean_data(df)
-df['image_url'] = df['images'].apply(get_image_url)
+df['image_url'] = df['image'].apply(get_image_url)
 
 kmeans = joblib.load("model/kmeans_model.pkl")
 tfidf = joblib.load("model/tfidf_vectorizer.pkl")
 
 # sidebar for user input
-
 st.sidebar.header("Pilih Anime Favorit")
-
 anime_title = st.sidebar.selectbox(
     "Plih Anime",
     df['title'].unique()
 )
 
 # Recommendation Logic
-
 selected_anime = df[df['title'] == anime_title].iloc[0]
 
 X_all = tfidf.transform(df['description'])
@@ -52,16 +46,14 @@ recommendations = df[
 ].head(6)
 
 # Output
-
 st.subheader("Rekomendasi Anime Serupa")
 
 cols = st.columns(3)
-
 for idx, row in recommendations.iterrows():
     with cols[idx % 3]:
         st.image(row['image_url'], use_container_width=True)
         st.markdown(f"### {row['title']}")
-        st.write(f"Type: {row['type']}")
-        st.write(f"Episodes: {row['episodes']}")
-        st.write(f"Release Year: {row['release_year']}")
+        st.write(f"Type: {row['Type']}")
+        st.write(f"Episodes: {row['Episodes']}")
+        st.write(f"Release Year: {row['Released_Year']}")
         st.write("---")
